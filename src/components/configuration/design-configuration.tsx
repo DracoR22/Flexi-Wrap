a69f4b54d688664ef98e707d187c461fabe40bcf
DracoR22 <rdraco039@gmail.com>
2024-05-12 20:45:38 -0500

    base 64 function completed

--- src/components/configuration/design-configuration.tsx
+++ src/components/configuration/design-configuration.tsx
@@ -1,18 +1,21 @@
 'use client'
 
-import Image from "next/image";
+import NextImage from "next/image";
 import { AspectRatio } from "../ui/aspect-ratio";
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
 import { Rnd } from "react-rnd"
 import HandleComponent from "./handle-component";
 import { ScrollArea } from "../ui/scroll-area";
 import { RadioGroup } from "@headlessui/react"
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator";
 import { Label } from "../ui/label";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
-import { CheckIcon, ChevronsUpDownIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon, ChevronsUpDownIcon } from "lucide-react";
+import { BASE_PRICE } from "@/config/products";
+import { useUploadThing } from "@/lib/uploadthing";
+import { toast } from "../ui/use-toast";
 
 interface Props {
     configId: string
@@ -28,12 +31,89 @@ const DesignConfigurator = ({ configId, imageDimensions, imageUrl }: Props) => {
       material: (typeof MATERIALS.options)[number];
       finish: (typeof FINISHES.options)[number] }>({ color: COLORS[0], model: MODELS.options[0], material: MATERIALS.options[0], finish: FINISHES.options[0] })
 
+    const [renderDimension, setRenderDimension] = useState({
+      width: imageDimensions.width / 4,
+      height: imageDimensions.height / 4
+    })
+
+    const [renderPosition, setRenderPosition] = useState({
+      x: 150,
+      y: 205
+    })
+
+    const phoneCaseRef = useRef<HTMLDivElement>(null)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    const { startUpload } = useUploadThing('imageUploader')
+
+    const saveConfiguration = async () => {
+       try {
+         const { left: caseLeft, top: caseTop, width, height } = phoneCaseRef.current!.getBoundingClientRect()
+
+         const { left: containerLeft, top: containerTop } = containerRef.current!.getBoundingClientRect()
+
+         const leftOffset = caseLeft - containerLeft
+         const topOffset =  caseTop - containerTop
+
+         const actualX = renderPosition.x - leftOffset
+         const actualY = renderPosition.y - topOffset
+
+         const canvas = document.createElement('canvas')
+         canvas.width = width
+         canvas.height = height
+
+         const ctx = canvas.getContext('2d')
+
+         const userImage = new Image()
+         userImage.crossOrigin = 'anonymous'
+         userImage.src = imageUrl
+
+         await new Promise((resolve) => (userImage.onload = resolve))
+
+         ctx?.drawImage(
+            userImage,
+            actualX,
+            actualY,
+            renderDimension.width,
+            renderDimension.height
+         )
+ 
+         const base64 = canvas.toDataURL()
+         const base64Data = base64.split(',')[1]
+         
+         const blob = base64ToBlob(base64Data, 'image/png')
+         const file = new File([blob], 'filename.png', { type: 'image/png' })
+
+         await startUpload([file], { configId })
+       } catch (error) {
+         toast({
+            title: 'Something went wrong!',
+            description: 'There was a problem saving your config, please try again.',
+            variant: 'destructive'
+         })
+       }
+    }
+
+    // Conver base64 to png
+    function base64ToBlob(base64: string, mimeType: string) {
+      const byteCharacters = atob(base64)
+      const byteNumbers = new Array(byteCharacters.length)
+
+      for (let i = 0; i < byteCharacters.length; i++) {
+         byteNumbers[i] = byteCharacters.charCodeAt(i)
+      }
+
+      const byteArray = new Uint8Array(byteNumbers)
+
+      return new Blob([byteArray], { type: mimeType })
+    }
+
   return (
-    <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
-      <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+      <div ref={containerRef} className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
          <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
-             <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
-                <Image alt="phone image" src={'/phone-template.png'} className="pointer-events-none z-50 select-none" fill/>
+             <AspectRatio ref={phoneCaseRef} ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
+                <NextImage alt="phone image" src={'/phone-template.png'} className="pointer-events-none z-50 select-none" fill/>
              </AspectRatio>
              {/* Square background */}
              <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]"/>
@@ -48,14 +128,32 @@ const DesignConfigurator = ({ configId, imageDimensions, imageUrl }: Props) => {
             bottomLeft: <HandleComponent/>,
             topRight: <HandleComponent/>,
             topLeft: <HandleComponent/>
-         }} className="absolute z-20 border-[3px] border-primary">
+         }} 
+         // Get coordinates and dimesions when resizing image
+          onResizeStop={(_, __, ref, ___, { x, y }) => {
+             setRenderDimension({
+               height: parseInt(ref.style.height.slice(0, -2)),
+               width: parseInt(ref.style.width.slice(0, -2))
+             })
+
+             setRenderPosition({ x, y })
+          }}
+
+          // Get coordinates and dimensions when drag and dropping image
+          onDragStop={(_, data) => {
+             const { x, y } = data
+
+             setRenderPosition({ x, y })
+          }}
+
+          className="absolute z-20 border-[3px] border-primary">
          <div className="relative w-full h-full">
-           <Image src={imageUrl} fill alt="your image" className="pointer-events-none"/>
+           <NextImage src={imageUrl} fill alt="your image" className="pointer-events-none"/>
          </div>
         </Rnd>
       </div>
 
-      <div className="h-[37.5rem] flex flex-col bg-white">
+      <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
           <ScrollArea className="relative flex-1 overflow-auto">
             <div aria-hidden="true" className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"/>
             <div className="px-8 pb-12 pt-8">
@@ -138,6 +236,12 @@ const DesignConfigurator = ({ configId, imageDimensions, imageUrl }: Props) => {
                                        ) : null}
                                     </span>
                                  </span>
+
+                                 <RadioGroup.Description as="span" className='mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right'>
+                                    <span className="font-medium text-gray-900">
+                                       {formatPrice(option.price / 100)}
+                                    </span>
+                                 </RadioGroup.Description>
                               </RadioGroup.Option>
                            ))}
                          </div>
@@ -147,6 +251,21 @@ const DesignConfigurator = ({ configId, imageDimensions, imageUrl }: Props) => {
                </div>
             </div>
           </ScrollArea>
+
+         <div className="w-full px-8 h-16 bg-white">
+            <div className="h-px w-full bg-zinc-200"/>
+             <div className="w-full h-full flex justify-end items-center">
+               <div className="w-full flex gap-6 items-center">
+                  <p className="font-medium whitespace-nowrap">
+                     {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
+                  </p>
+                  <Button onClick={() => saveConfiguration()} size={'sm'} className="w-full">
+                     Continue
+                     <ArrowRightIcon className="h-4 w-4 ml-1.5 inline"/>
+                  </Button>
+               </div>
+             </div>
+         </div>
       </div>
     </div>
   )
